b8b9e4aada7f54270e78275dc4b9f3f61c72c78e
Fahad Heylaal <fahad19@gmail.com>
2023-07-30 16:02:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# featurevisor-example-expressjs
+
+Example of using [Featurevisor](https://featurevisor.com/) with [Express.js](https://expressjs.com/) application.
+
+# Installation
+
+Clone the repository and install dependencies:
+
+```
+$ npm ci
+```
+
+# Usage
+
+Run the application:
+
+```
+$ npm start
+```
+
+Visit [http://localhost:3000](http://localhost:3000) to see the application.
+
+# License
+
+MIT © [Fahad Heylaal](https://fahad19.com)
---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const { createInstance } = require("@featurevisor/sdk");
+
+require("isomorphic-fetch");
+
+/**
+ * Constants
+ */
+const PORT = 3000;
+const REFRESH_INTERVAL = 60 * 5; // every 5 minutes
+
+const DATAFILE_URL =
+  process.env.environment === "production"
+    ? "https://featurevisor-example-cloudflare.pages.dev/production/datafile-tag-all.json"
+    : "https://featurevisor-example-cloudflare.pages.dev/staging/datafile-tag-all.json";
+
+/**
+ * Featurevisor instance
+ */
+const f = createInstance({
+  datafileUrl: DATAFILE_URL,
+
+  onReady: () => console.log(`Featurevisor SDK is now ready`),
+  onRefresh: () => console.log(`Featurevisor SDK has refreshed`),
+  onUpdate: () => console.log(`Featurevisor SDK has updates`),
+
+  // optionally refresh the datafile every 5 minutes,
+  // without having to restart the server
+  refreshInterval: REFRESH_INTERVAL,
+});
+
+/**
+ * Express app with middleware
+ */
+const app = express();
+
+app.use((req, res, next) => {
+  req.f = f;
+  next();
+});
+
+/**
+ * Routes
+ */
+app.get("/", (req, res) => {
+  const featureKey = "baz";
+  const context = { userId: "user-123" };
+
+  const isEnabled = f.isEnabled(featureKey, context);
+
+  if (isEnabled) {
+    res.send("Hello World!");
+  } else {
+    res.send("Not enabled yet!");
+  }
+});
+
+/**
+ * Start the server
+ */
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
